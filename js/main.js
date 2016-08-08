87d420008b22f3d383c5e25f52b08844e41df190
Waleed Mansour <Waleed Mansour>
2016-08-08 15:32:35 -0500

    Add other pages. Complete phtographs and emails
    Did not finish submit button for contact

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-// $(document).ready(function() {
-//   $('#submit-btn').click(function() {
-//     $('#contact-form').attr('action','mailto:wmans619@gmail.com?subject=' + $('#name').val() + '&body=' + $('#email').val() + $('#message').val());
-//     $('#contact-form').submit();
-//   });
-// });
-
-
 $(function() {
   $('#home-link').click(function(){
     $('html,body').animate({scrollTop: $('header').offset().top},'slow')
@@ -18,5 +10,53 @@ $(function() {
   })
   $('#contact-link').click(function(){
     $('html,body').animate({scrollTop: $('.contact').offset().top},'slow')
+  })
+})
+
+
+
+// Contact Form
+
+// $(function() {
+//   $(".button").click(function() {
+//
+//   });
+// });
+
+$(function() {
+  $('.error').hide();
+  $('.button').click(function() {
+    // validate and process form here
+    $('.error').hide();
+    var name = $('input#name').val();
+    if (name ==="") {
+      $('label#name_error').show();
+      $('input#name').focus();
+      return false;
+    }
+    var email = $('input#email').val();
+    if (email ==="") {
+      $('label#email_error').show();
+      $('input#email').focus();
+      return false;
+    }
+    var message = $('textarea#message').val();
+    if (message =="") {
+      $('label#message_error').show();
+      $('textarea#message').focus();
+      return false;
+    }
+
+    var dataString = "name=" + name +'&email=' + email + '&message' + message;
+    $.ajax({
+      type: 'POST',
+      success: function() {
+        $('#contact_form').html('<div id="msg"></div>');
+        $('#msg').html('<h2>Contact Form Submitted!</h2>')
+        .append('<p>We will be in touch soon. </p>')
+        .hide()
+      }
+    });
+    return false;
   })
 })
