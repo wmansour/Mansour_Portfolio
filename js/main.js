167169e4e8be9ffeaec162d788ea6eefe2c2130d
Waleed Mansour <Waleed Mansour>
2016-08-08 12:23:32 -0500

    Finish layout of landing page and responsiveness.
    about to attempt submit button functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-$(document).ready(function() {
-  $('#submit-btn').click(function() {
-    $('#contact-form').attr('action','mailto:wmans619@gmail.com?subject=' + $('#name').val() + '&body=' + $('#email').val() + $('#message').val());
-    $('#contact-form').submit();
-  });
-});
+// $(document).ready(function() {
+//   $('#submit-btn').click(function() {
+//     $('#contact-form').attr('action','mailto:wmans619@gmail.com?subject=' + $('#name').val() + '&body=' + $('#email').val() + $('#message').val());
+//     $('#contact-form').submit();
+//   });
+// });
+
+
+$(function() {
+  $('#home-link').click(function(){
+    $('html,body').animate({scrollTop: $('header').offset().top},'slow')
+  })
+  $('#about-link').click(function(){
+    $('html,body').animate({scrollTop: $('.about').offset().top},'slow')
+  })
+  $('#portfolio-link').click(function(){
+    $('html,body').animate({scrollTop: $('.portfolio').offset().top},'slow')
+  })
+  $('#contact-link').click(function(){
+    $('html,body').animate({scrollTop: $('.contact').offset().top},'slow')
+  })
+})
